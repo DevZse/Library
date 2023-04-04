0e5ba6b6ed6bfc41489cbb9740c839523c7d9c01
DevZse <85144939+DevZse@users.noreply.github.com>
2023-04-04 01:20:10 -0400

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -1,21 +1,17 @@
 local httpService = game:GetService('HttpService')
 
 local ThemeManager = {} do
-	ThemeManager.Folder = 'LinoriaLibSettings'
-	-- if not isfolder(ThemeManager.Folder) then makefolder(ThemeManager.Folder) end
-
+	ThemeManager.Folder = 'Extreme Edge'
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"0055ff","BackgroundColor":"141414","OutlineColor":"323232"}') },
-		['Dracula'] = { 2, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"232533","AccentColor":"6271a5","BackgroundColor":"1b1c27","OutlineColor":"7c82a7"}') },
-		['Bitch Bot'] = { 3, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}') },
-		['Kiriot Hub'] = { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"30333b","AccentColor":"ffaa00","BackgroundColor":"1a1c20","OutlineColor":"141414"}') },
-		['Fatality'] = { 5, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}') },
-		['Green'] = { 6, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"141414","AccentColor":"00ff8b","BackgroundColor":"1c1c1c","OutlineColor":"3c3c3c"}') },
-		['Jester'] = { 7, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
-		['Mint'] = { 8, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"242424","AccentColor":"3db488","BackgroundColor":"1c1c1c","OutlineColor":"373737"}') },
-		['Tokyo Night'] = { 9, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"323232"}') },
-		['Ubuntu'] = { 10, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"3e3e3e","AccentColor":"e2581e","BackgroundColor":"323232","OutlineColor":"191919"}') },
+		['Default'] = { 1, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'1c1c1c','AccentColor':'0055ff','BackgroundColor':'141414','OutlineColor':'323232'}') },
+		['BBot'] = { 2, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'1e1e1e','AccentColor':'7e48a3','BackgroundColor':'232323','OutlineColor':'141414'}') },
+		['Fatality'] = { 3, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'1e1842','AccentColor':'c50754','BackgroundColor':'191335','OutlineColor':'3c355d'}') },
+		['Jester'] = { 4, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'242424','AccentColor':'db4467','BackgroundColor':'1c1c1c','OutlineColor':'373737'}') },
+		['Mint'] = { 5, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'242424','AccentColor':'3db488','BackgroundColor':'1c1c1c','OutlineColor':'373737'}') },
+		['Tokyo Night'] = { 6, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'191925','AccentColor':'6759b3','BackgroundColor':'16161f','OutlineColor':'323232'}') },
+		['Ubuntu'] = { 7, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'3e3e3e','AccentColor':'e2581e','BackgroundColor':'323232','OutlineColor':'191919'}') },
+		['Quartz'] = { 8, httpService:JSONDecode('{'FontColor':'ffffff','MainColor':'232330','AccentColor':'426e87','BackgroundColor':'1d1b26','OutlineColor':'27232f'}') },
 	}
 
 	function ThemeManager:ApplyTheme(theme)
@@ -39,11 +35,13 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:ThemeUpdate()
-		self.Library.FontColor = Options.FontColor.Value
-		self.Library.MainColor = Options.MainColor.Value
-		self.Library.AccentColor = Options.AccentColor.Value
-		self.Library.BackgroundColor = Options.BackgroundColor.Value
-		self.Library.OutlineColor = Options.OutlineColor.Value
+		-- This allows us to force apply themes without loading the themes tab :)
+		local options = { 'FontColor', 'MainColor', 'AccentColor', 'BackgroundColor', 'OutlineColor' }
+		for i, field in next, options do
+			if Options and Options[field] then
+				self.Library[field] = Options[field].Value
+			end
+		end
 
 		self.Library.AccentColorDark = self.Library:GetDarkerColor(self.Library.AccentColor);
 		self.Library:UpdateColorsUsingRegistry()
@@ -166,7 +164,7 @@ local ThemeManager = {} do
 		end
 
 		local theme = {}
-		local fields = { "FontColor", "MainColor", "AccentColor", "BackgroundColor", "OutlineColor" }
+		local fields = { 'FontColor', 'MainColor', 'AccentColor', 'BackgroundColor', 'OutlineColor' }
 
 		for _, field in next, fields do
 			theme[field] = Options[field].Value:ToHex()
